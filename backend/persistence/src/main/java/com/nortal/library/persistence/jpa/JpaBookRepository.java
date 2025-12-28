007ed4de351ac656d978ad8c1124072f15eb9a92
kristiinalokko <124159396+kristiinalokko@users.noreply.github.com>
2025-12-28 13:38:47 +0200

    Fixed a bug

--- backend/persistence/src/main/java/com/nortal/library/persistence/jpa/JpaBookRepository.java
+++ backend/persistence/src/main/java/com/nortal/library/persistence/jpa/JpaBookRepository.java
@@ -1,11 +1,9 @@
 package com.nortal.library.persistence.jpa;
 
 import com.nortal.library.core.domain.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface JpaBookRepository extends JpaRepository<Book, String> {
-    List<Book> findByLoanedTo(String memberId);
-
+  List<Book> findByLoanedTo(String memberId);
 }
